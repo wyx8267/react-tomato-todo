9655e24190df0023f112c982b6203a9f66c152a9
wyx8267 <369264448@qq.com>
2019-09-10 11:35:11 +0800

    Updates

---
+++ precache-manifest.49ed862729d3ae92300efc0cfb3e16d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cfac66d1cefe770f45d21e65133c96",
+    "url": "/react-tomato-todo/index.html"
+  },
+  {
+    "revision": "7e06e6762d02e19351cc",
+    "url": "/react-tomato-todo/static/css/2.c850e9dc.chunk.css"
+  },
+  {
+    "revision": "5535976f11021236d8fc",
+    "url": "/react-tomato-todo/static/css/main.ef6b6d21.chunk.css"
+  },
+  {
+    "revision": "7e06e6762d02e19351cc",
+    "url": "/react-tomato-todo/static/js/2.3b2496a6.chunk.js"
+  },
+  {
+    "revision": "5535976f11021236d8fc",
+    "url": "/react-tomato-todo/static/js/main.f6ac0c26.chunk.js"
+  },
+  {
+    "revision": "87c6d71e6f5b939da07c",
+    "url": "/react-tomato-todo/static/js/runtime~main.2b81159f.js"
+  }
+]);
